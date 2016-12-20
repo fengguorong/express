fa82b1c9c447574f4e09d7aa23ccb8e1ee509d98
codejianqxr <1046396106@qq.com>
2016-12-20 17:34:52 +0800

    add route

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ var express=require("./express.js");
 var app=express("abc");
 app.listen(8888);
 
-
-app.set("/",function(req,res){
-    res.send("跟目录");
-})
-app.set("/abc",function(req,res){
+app.set("/abc/",function(req,res){
     res.send("abc");
 })
-
-app.set("/aaa",function(req,res){
-    res.send("aaa");
+app.set("/abc/:id",function(req,res){
+    res.send(res.param.id);
 })
+
+
+
